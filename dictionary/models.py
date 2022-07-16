403423fdbc6fcc7ed9af59d099e6fbb1dd44aeb3
shewen.davelaar@new10.com <shewen.davelaar@new10.com@NEW10-C02DW3TEMD6T-shewen.davelaar>
2022-07-16 18:40:09 +0200

    updated database

--- dictionary/models.py
+++ dictionary/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Words(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
-    language = models.ForeignKey('Languages', on_delete=models.CASCADE)
-    word = models.CharField(max_length=1000)
-    meaning= models.CharField(max_length=1000)
 
 class Languages(models.Model):  
     name = models.CharField(max_length=100)
