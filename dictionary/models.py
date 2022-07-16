63afb31bb8272ed2ab4fa2cde5a4496372331c4c
shewen.davelaar@new10.com <shewen.davelaar@new10.com@NEW10-C02DW3TEMD6T-shewen.davelaar>
2022-07-16 18:36:31 +0200

    updated database

--- dictionary/models.py
+++ dictionary/models.py
@@ -9,3 +9,9 @@ class Words(models.Model):
 
 class Languages(models.Model):  
     name = models.CharField(max_length=100)
+
+class Word_list(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    word = models.CharField(max_length=1000)
+    meaning= models.CharField(max_length=1000)
+    language = models.ForeignKey('Languages', on_delete=models.CASCADE)
