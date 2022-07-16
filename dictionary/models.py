5773bc06454f33c27cbf7e13b4791812be731946
shewen.davelaar@new10.com <shewen.davelaar@new10.com@NEW10-C02DW3TEMD6T-shewen.davelaar>
2022-07-16 18:47:25 +0200

    updated database

--- dictionary/models.py
+++ dictionary/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Languages(models.Model):  
     name = models.CharField(max_length=100)
-    country = models.CharField(max_length=100, null = True)
 
 class Word_list(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
