11f4f524753b59be2e06eeaf7b93e7db603c3a94
shewen.davelaar@new10.com <shewen.davelaar@new10.com@NEW10-C02DW3TEMD6T-shewen.davelaar>
2022-07-17 01:05:52 +0200

    encreased the limit on characters words model

--- dictionary/models.py
+++ dictionary/models.py
@@ -8,5 +8,5 @@ class Languages(models.Model):
 class Words(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
     word = models.CharField(max_length=500)
-    meaning= models.CharField(max_length=2000)
+    meaning= models.CharField(max_length=2500)
     language = models.ForeignKey('Languages', on_delete=models.CASCADE)
