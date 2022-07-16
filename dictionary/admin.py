833ed30b59e4e750f6dc58bff77740f6acaee017
shewen.davelaar@new10.com <shewen.davelaar@new10.com@NEW10-C02DW3TEMD6T-shewen.davelaar>
2022-07-16 18:52:31 +0200

    updated database

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Languages
+from .models import Languages, Words
 
 # Register your models here.
 
--- dictionary/models.py
+++ dictionary/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Languages(models.Model):  
     name = models.CharField(max_length=100)
 
-class Word(models.Model):
+class Words(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
     word = models.CharField(max_length=1000)
     meaning= models.CharField(max_length=1000)
